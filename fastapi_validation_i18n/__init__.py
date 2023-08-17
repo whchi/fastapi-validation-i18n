e3942de3fbc0c9c624eab714ad4c65aae6bf96e4
whc <whccchi@gmail.com>
2023-08-17 11:54:04 +0800

    add github statics

--- fastapi_validation_i18n/__init__.py
+++ fastapi_validation_i18n/__init__.py
@@ -2,5 +2,5 @@ from .handler import i18n_exception_handler
 from .middleware import I18nMiddleware
 from .translator import Translator
 
-__version__ = '0.1.61'
+__version__ = '0.3.1'
 __all__ = ['Translator', 'i18n_exception_handler', 'I18nMiddleware']
