8019ff45cc259e8749ed8c1db52ece91472b18cc
whc <whccchi@gmail.com>
2023-12-10 22:42:43 +0800

    0.4.0

--- fastapi_validation_i18n/__init__.py
+++ fastapi_validation_i18n/__init__.py
@@ -2,5 +2,5 @@ from .handler import i18n_exception_handler
 from .middleware import I18nMiddleware
 from .translator import Translator
 
-__version__ = '0.3.4'
+__version__ = '0.4.0'
 __all__ = ['Translator', 'i18n_exception_handler', 'I18nMiddleware']
